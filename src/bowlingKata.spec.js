e5aaf3ed15bb2d9f372a55e6e17d1e6cd618f52d
Jonathan <jonathan.moruno@ucb.edu.bo>
2024-03-13 11:32:27 -0400

    Se refactorizo el codigo del archivo .spc haciendoque la funcion rollMany este antes de las dos pruebas que la estan usando

--- src/bowlingKata.spec.js
+++ src/bowlingKata.spec.js
@@ -5,17 +5,17 @@ describe("bowlingKata", () => {
     const myGame = new Game();
     expect(myGame).toBeDefined();
   });
-  const g = new Game();
-  it("Debería tirar 20 veces el jugador derribando cero pinos en cada tiro obteniendo un score de 0", () => {
-    rollMany(20,0)
-    expect(g.score()).toEqual(0);
-  });
   function rollMany(n,pins){
     for (let i=0;i<n;i++)
     {
       g.roll(pins);
     }
   }
+  const g = new Game();
+  it("Debería tirar 20 veces el jugador derribando cero pinos en cada tiro obteniendo un score de 0", () => {
+    rollMany(20,0)
+    expect(g.score()).toEqual(0);
+  });
   it("Debería tirar 20 veces el jugador derribando un pino en cada tiro obteniendo un score de 20", () => {
     
     rollMany(20,1)
