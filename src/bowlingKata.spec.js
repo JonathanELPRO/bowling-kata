9412cdd5ce6a1c47dcc8164c85709566525b2de7
Jonathan <jonathan.moruno@ucb.edu.bo>
2024-03-13 11:25:59 -0400

    Se refactorizo el codigo de la segunda pueba haciendo que la cantidad de tiros sea un numero variable y tambien la cantidad de pinos derribados

--- src/bowlingKata.spec.js
+++ src/bowlingKata.spec.js
@@ -5,16 +5,18 @@ describe("bowlingKata", () => {
     const myGame = new Game();
     expect(myGame).toBeDefined();
   });
+  const g = new Game();
   it("Debería tirar 20 veces el jugador derribando cero pinos en cada tiro obteniendo un score de 0", () => {
-    const g = new Game();
-    for (let i=0;i<20;i++)
+    let n=20
+    let pins=0
+    for (let i=0;i<n;i++)
     {
-      g.roll(0);
+      g.roll(pins);
     }
     expect(g.score()).toEqual(0);
   });
   it("Debería tirar 20 veces el jugador derribando un pino en cada tiro obteniendo un score de 20", () => {
-    const g = new Game();
+    
     for (let i=0;i<20;i++)
     {
       g.roll(1);
