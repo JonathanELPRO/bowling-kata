b72f041f25d16e3206e7bc0053cee9d808707c72
Jonathan <jonathan.moruno@ucb.edu.bo>
2024-03-13 11:22:00 -0400

    Caso de prueba donde se tiran 20 tiros sacando uno  en todos y nuestro score nos retorna que es 20

--- src/bowlingKata.spec.js
+++ src/bowlingKata.spec.js
@@ -5,12 +5,20 @@ describe("bowlingKata", () => {
     const myGame = new Game();
     expect(myGame).toBeDefined();
   });
-  it("Debería crearse la clase juego pero vacía", () => {
+  it("Debería tirar 20 veces el jugador derribando cero pinos en cada tiro obteniendo un score de 0", () => {
     const g = new Game();
     for (let i=0;i<20;i++)
     {
       g.roll(0);
     }
     expect(g.score()).toEqual(0);
+  });
+  it("Debería tirar 20 veces el jugador derribando un pino en cada tiro obteniendo un score de 20", () => {
+    const g = new Game();
+    for (let i=0;i<20;i++)
+    {
+      g.roll(1);
+    }
+    expect(g.score()).toEqual(20);
   });
 });
