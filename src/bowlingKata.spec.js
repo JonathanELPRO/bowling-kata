c8c6762f43d2ad67ea596c2b1748aef32c5ca4e2
Jonathan <jonathan.moruno@ucb.edu.bo>
2024-03-13 11:29:06 -0400

    Se refactorizo el codigo de la segunda pueba haciendo que el for que tiraba los tros y sumaba puntos al score este en una funcion aparte llamada rollMany

--- src/bowlingKata.spec.js
+++ src/bowlingKata.spec.js
@@ -9,12 +9,15 @@ describe("bowlingKata", () => {
   it("Debería tirar 20 veces el jugador derribando cero pinos en cada tiro obteniendo un score de 0", () => {
     let n=20
     let pins=0
+    rollMany(n,pins)
+    expect(g.score()).toEqual(0);
+  });
+  function rollMany(n,pins){
     for (let i=0;i<n;i++)
     {
       g.roll(pins);
     }
-    expect(g.score()).toEqual(0);
-  });
+  }
   it("Debería tirar 20 veces el jugador derribando un pino en cada tiro obteniendo un score de 20", () => {
     
     for (let i=0;i<20;i++)
